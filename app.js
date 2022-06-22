6974fde68ec29fb25c05c76e8e3c3fda0ec71594
tatamibox <tat4mi@gmail.com>
2022-06-21 17:03:29 -0700

    button updates dom with new quotes

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+
+// fetches the kanye quote from kanye rest api
 const getKanyeQuote = async () => {
 
     const kanyeQuote = await axios.get('https://api.kanye.rest');
-    return (kanyeQuote.data.quote);
+    return (kanyeQuote.data.quote)
+}
+//
+getKanyeQuote();
+
+//function to change the main quote to the generated one on button click
+
+const replaceQuote = async () => {
+    const newQuote = await getKanyeQuote();
+    mainQuote.innerHTML = `"${newQuote}"`;
 
 }
 
+
+//
+
+
+//selecting button/important paragraphs for proceeding onclick events
 const button = document.querySelector('button');
+const mainQuote = document.querySelector('.mainQuote');
+button.addEventListener('click', () => {
+    replaceQuote();
+})
 
 
 
