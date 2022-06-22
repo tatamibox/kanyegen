782dc6167e0abd57050f442d4dc6048bbab598ad
tatamibox <tat4mi@gmail.com>
2022-06-21 17:23:46 -0700

    added 5 quotes history on screen at once

--- app.js
+++ app.js
@@ -6,18 +6,47 @@ const getKanyeQuote = async () => {
     return (kanyeQuote.data.quote)
 }
 //
-getKanyeQuote();
+
+// appending Old quotes to the body
+
+let counter = 0;
+
+const pastQuotesSection = document.querySelector('.pastQuotesSection');
+const appendOldQuotes = () => {
+    counter += 1;
+
+    if (counter <= 5) {
+        const old = document.createElement('div');
+        old.innerHTML = `"${mainQuote.innerHTML}"`;
+        pastQuotesSection.appendChild(old);
+    } else if (counter > 5) {
+
+        const firstChild = pastQuotesSection.getElementsByTagName('div')[0];
+        pastQuotesSection.removeChild(firstChild);
+        const old = document.createElement('div');
+        old.innerHTML = `"${mainQuote.innerHTML}"`;
+        pastQuotesSection.appendChild(old);
+    }
+
+
+
+
+}
+//
 
 //function to change the main quote to the generated one on button click
 
 const replaceQuote = async () => {
     const newQuote = await getKanyeQuote();
+    appendOldQuotes();
     mainQuote.innerHTML = `"${newQuote}"`;
+
 
 }
 
 
 //
+
 
 
 //selecting button/important paragraphs for proceeding onclick events
